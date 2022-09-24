72a3b090aec5951e6145f5ae2a51e15d0a3e5e3b
starkat7 <kaelbe@hotmail.com>
2022-09-24 14:59:12 -0700

    More Signup work

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -5,16 +5,17 @@ import userService from "../../utils/userService";
 import { useNavigate } from "react-router-dom";
 
 import { useForm } from '@mantine/form';
-import { TextInput, Button, Box, Text, Container } from '@mantine/core';
+import { TextInput, PasswordInput, Button, Box, Text, Container } from '@mantine/core';
 
 
 export default function SignUpPage({ handleSignUpOrLogin }) {
 
+  // ------------- State Stuff -------------
   const [error, setError] = useState({
-    message: '',
+    message: "",
     passwordError: false
   });
-  
+
   const [state, setState] = useState({
     username: "",
     email: "",
@@ -22,31 +23,64 @@ export default function SignUpPage({ handleSignUpOrLogin }) {
     passwordConf: "",
   });
 
+  // ------------- Handlers -------------
+  function handleChange(e) {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  // ------------- Mantine Form Stuff -------------
   const form = useForm({
-    initialValues: { name: '', email: '', password: '' },
+    initialValues: { name: "", email: "", password: "", passwordConf: "" },
 
     validate: {
       name: (value) => (value.length < 2 ? 'Name must have at least 2 letters' : null),
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email')
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+      confirmPassword: (value, values) =>
+        value !== values.password ? 'Passwords did not match' : null,
     },
   });
 
+  // ------------- Here's the return -------------
   return (
-      <Box sx={{ maxWidth: 450 }} mx="auto" my="auto" color="grey">
+    <Box sx={{ maxWidth: 450 }} mx="auto" my="auto" color="grey">
+      <Container sx={{ maxWidth: 400 }}>
         <Text mt="sm" align="center" size="xl" weight={700} my="sm">My Pokemon Journal</Text>
         <Text size="md" my="sm">One place to track what's going on in all your Pokemon games.</Text>
-        <Container>
-          <form onSubmit={form.onSubmit(console.log)}>
-            <TextInput label="Name" placeholder="Name" {...form.getInputProps('name')} />
-            <TextInput mt="sm" label="Email" placeholder="Email" {...form.getInputProps('email')} />
-            <TextInput mt="sm" label="Password" placeholder="Password" {...form.getInputProps('password')} />
-            <Container align="center">
-              <Button type="submit" mt="sm">
-                Submit
-              </Button>
-            </Container>
-          </form>
-        </Container>
-      </Box>
+      </Container>
+      <Container>
+        <form onSubmit={form.onSubmit(() => handleSignUpOrLogin)}>
+          <TextInput label="Name"
+            placeholder="Name"
+            onChange={handleChange}
+            {...form.getInputProps('name')} />
+          <TextInput
+            mt="sm"
+            label="Email"
+            placeholder="Email"
+            onChange={handleChange}
+            {...form.getInputProps('email')} />
+          <PasswordInput
+            mt="sm"
+            label="Password"
+            placeholder="Password"
+            onChange={handleChange}
+            {...form.getInputProps('password')} />
+          <PasswordInput
+            mt="sm"
+            label="Confirm Password"
+            placeholder="Password"
+            onChange={handleChange}
+            {...form.getInputProps('passwordConf')} />
+          <Container align="center">
+            <Button type="submit" mt="sm" my="md">
+              Submit
+            </Button>
+          </Container>
+        </form>
+      </Container>
+    </Box>
   );
 }
