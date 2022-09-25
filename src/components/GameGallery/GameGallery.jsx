ed5b52049eb9b6e46260eac11a74b19201729fa0
starkat7 <kaelbe@hotmail.com>
2022-09-25 14:56:51 -0700

    Stub up AddGame component

---
+++ src/components/GameGallery/GameGallery.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Card } from "semantic-ui-react";
+
+export default function GameGallery(props) {
+    return (
+        <h1>Showing all games as cards</h1>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import PageHeader from "../../components/Header/Header";
 import Game from "../../components/Game/Game";
+import { Header } from "semantic-ui-react"
 
 export default function Home({ loggedInUser, handleLogout }) {
     return (
         <>
             <PageHeader loggedInUser={loggedInUser} handleLogout={handleLogout} />
-            <h1>My Games at a Glance, from the Home Page now</h1>
+            <Header as="h1" textAlign="center">My Games</Header>
+            <div className="ui section divider"></div>
             <Game />
         </>
     )
