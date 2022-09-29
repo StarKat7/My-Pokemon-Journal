15b1149fbd5c96bf481b6c78608c25ab8b625bf9
starkat7 <kaelbe@hotmail.com>
2022-09-29 11:51:41 -0700

    Console logs in the deleteGame controller are firing but it isn't deleting the game and tasks from the database

--- controllers/games.js
+++ controllers/games.js
@@ -1,52 +1,58 @@
 const Game = require("../models/game");
+const Task = require("../models/task");
 const User = require("../models/user");
 
 module.exports = {
-    create,
-    userGames,
-    deleteGame
+  create,
+  userGames,
+  deleteGame
 }
 
 async function deleteGame(req, res) {
   console.log("Arrived at the deleteGame controller")
   try {
-
+    await Task.deleteMany({ game: req.params._id });
+    console.log("Tasks were deleted");
+    await Game.findOneAndDelete({ _id: req.params._id });
+    console.log("Game was deleted")
+    res.json({data: "Game and tasks were removed"})
   } catch (err) {
-    
+    console.log("Something went wrong in the deleteGame controller")
+    res.status(400).json({ error: err })
   }
 }
 
 async function userGames(req, res) {
-    try {
-      // So to get the logged-in user I use req.user._id?
+  try {
+    // So to get the logged-in user I use req.user._id?
     //   const user = await User.findOne({ user: req.user._id });
     //   if (!user) return res.status(404).json({ error: "Something's gone wrong in userGames, check it in controllers/users.js" })
-      // Now I find the user's games...
-      // I put user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'} in the Game model so that should apply here
-      const games = await Game.find({ user: req.user._id }).populate("tasksToDo").populate("tasksDone").exec();
-      res.status(200).json({
-        data: games
-      });
-    } catch (err) {
-      console.log(err.message, " <- this error is occurring in the userGames function in the users controller");
-      res.status(400).json({ error: "Something went wrong..." });
-    }
+    // Now I find the user's games...
+    // I put user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'} in the Game model so that should apply here
+    const games = await Game.find({ user: req.user._id }).populate("tasksToDo").populate("tasksDone").exec();
+    res.status(200).json({
+      data: games
+    });
+  } catch (err) {
+    console.log(err.message, " <- this error is occurring in the userGames function in the users controller");
+    res.status(400).json({ error: "Something went wrong..." });
   }
+}
 
 async function create(req, res) {
-    console.log(req.body, req.user); //Checking the user and making sure the middleware fired off successfully
-    try {
-        // So I'm using the Game model to create a document in mongodb's games collection
-        const game = await Game.create({
-            user: req.user,
-            gameTitle: req.body.title,
-            gen: req.body.gen,
-            coverUrl: req.body.coverUrl,
-            platform: req.body.platform
-        });
-        // Now respond to the client with the data and a status 201
-        res.status(201).json({ data: game })
-    } catch (err) {
+  console.log(req.body, req.user); //Checking the user and making sure the middleware fired off successfully
+  try {
+    // So I'm using the Game model to create a document in mongodb's games collection
+    const game = await Game.create({
+      user: req.user,
+      gameTitle: req.body.title,
+      gen: req.body.gen,
+      coverUrl: req.body.coverUrl,
+      platform: req.body.platform
+    });
+    // Now respond to the client with the data and a status 201
+    res.status(201).json({ data: game })
+  } catch (err) {
 
-    }
+  }
 }
