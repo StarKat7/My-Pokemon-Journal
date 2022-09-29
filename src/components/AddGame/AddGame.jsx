64fef2e8055e1b54f65fbfe22bb40ed0df9de654
starkat7 <kaelbe@hotmail.com>
2022-09-29 16:40:29 -0700

    Dropdown is functional for Add Game

--- src/components/AddGame/AddGame.jsx
+++ src/components/AddGame/AddGame.jsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
-import { Card, Modal, Dropdown, Form, Button } from "semantic-ui-react";
+import { Card, Modal, Dropdown, Form, Button, Divider } from "semantic-ui-react";
 
 
 // ------------- Games for the Dropdown -------------
 // Gotta figure this out... Semantic UI has a certain way of doing it that I have to figure out how to use.
 // Actually I'll start with allowing it to be self-entered, then figure out the dropdowns later. Just make sure the form WORKS before you get fancy with it!
+// Back to figure out the dropdown... If selecting a game just changes the state, that would work wouldn't it? Let's try it out...
 const availableGames = [
-    { gameTitle: "Pokemon Sword", gen: 8, coverurl: "https://assets.pokemon.com/assets//cms2/img/video-games/video-games/pokemon_sword_shield/where-to-buy-sword-169-en.jpg", platform: "Nintendo Switch" },
-    { gameTitle: "Pokemon Shield", gen: 8, coverurl: "https://assets.pokemon.com/assets//cms2/img/video-games/video-games/pokemon_sword_shield/where-to-buy-shield-169-en.jpg", platform: "Nintendo Switch" },
-    { gameTitle: "Pokemon Legends: Arceus", gen: 8, coverurl: "https://assets.pokemon.com/assets/cms2/img/video-games/video-games/pokemon_legends_arceus/pokemon-legends-arceus-box-art-en.jpg", platform: "Nintendo Switch" },
-    { gameTitle: "Pokemon Sun", gen: 7, coverurl: "", platform: "3DS" },
-    { gameTitle: "Pokemon Moon", gen: 7, coverurl: "", platform: "3DS" },
-    { gameTitle: "Pokemon UltraSun", gen: 7, coverurl: "https://assets.pokemon.com/assets/cms2/img/video-games/video-games/pokemon_ultra_sun_ultra_moon/art/pcom-sorghselrkjhgserjgt-slider-01.jpg", platform: "3DS" },
-    { gameTitle: "Pokemon UltraMoon", gen: 7, coverurl: "https://assets.pokemon.com/assets/cms2/img/video-games/video-games/pokemon_ultra_sun_ultra_moon/art/pcom-sorghselrkjhgserjgt-slider-02.jpg", platform: "3DS" },
+    { title: "Pokemon Sword", gen: 8, coverUrl: "https://assets.pokemon.com/assets//cms2/img/video-games/video-games/pokemon_sword_shield/where-to-buy-sword-169-en.jpg", platform: "Nintendo Switch" },
+    { title: "Pokemon Shield", gen: 8, coverUrl: "https://assets.pokemon.com/assets//cms2/img/video-games/video-games/pokemon_sword_shield/where-to-buy-shield-169-en.jpg", platform: "Nintendo Switch" },
+    { title: "Pokemon Legends: Arceus", gen: 8, coverUrl: "https://assets.pokemon.com/assets/cms2/img/video-games/video-games/pokemon_legends_arceus/pokemon-legends-arceus-box-art-en.jpg", platform: "Nintendo Switch" },
+    { title: "Pokemon Sun", gen: 7, coverUrl: "https://assets.pokemon.com/assets/cms2/img/video-games/video-games/pokemon_sun_moon/slider-solgaleo.jpg", platform: "3DS" },
+    { title: "Pokemon Moon", gen: 7, coverUrl: "https://assets.pokemon.com/assets/cms2/img/video-games/video-games/pokemon_sun_moon/slider-lunala.jpg", platform: "3DS" },
+    { title: "Pokemon UltraSun", gen: 7, coverUrl: "https://assets.pokemon.com/assets/cms2/img/video-games/video-games/pokemon_ultra_sun_ultra_moon/art/pcom-sorghselrkjhgserjgt-slider-01.jpg", platform: "3DS" },
+    { title: "Pokemon UltraMoon", gen: 7, coverUrl: "https://assets.pokemon.com/assets/cms2/img/video-games/video-games/pokemon_ultra_sun_ultra_moon/art/pcom-sorghselrkjhgserjgt-slider-02.jpg", platform: "3DS" },
 ]
 
+// So I have to use these in the actual dropdown element, and then I can match the game's name to the title and change the state?
 const gameOptions = [
     { key: "Sword", text: "Pokemon Sword", value: "Pokemon Sword" },
     { key: "Shield", text: "Pokemon Shield", value: "Pokemon Shield" },
@@ -27,7 +29,7 @@ const gameOptions = [
 
 export default function AddGame({ handleAddGame }) {
     // AddGame will be a form that is rendered on the Home page, accordian so it only pops out when the user needs it.
-    
+
     // ------------- States -------------
     const [dropdownGame, setDropdownGame] = useState({})
     const [gameForm, setGameForm] = useState({
@@ -44,6 +46,15 @@ export default function AddGame({ handleAddGame }) {
             ...gameForm,
             [e.target.name]: e.target.value,
         });
+    }
+
+    function handleClose(e, data) {
+        // So I think I can use .find() to use the option's name to find the availableGame with the corresponding name? Then set the state... Let's give it a try.
+        const selectedGame = availableGames.find(({title}) => title === data.value);
+        console.log(selectedGame);
+        // Okay successfully grabbed the game from the availableGames array...
+        setGameForm(selectedGame);
+        // IT WORKED??
     }
 
     function handleSubmit(e) {
@@ -72,8 +83,16 @@ export default function AddGame({ handleAddGame }) {
                 trigger={<Button size="large">Add New Game</Button>}
             >
                 <Modal.Header>Add Game</Modal.Header>
-                <Modal.Content image>
+                <Modal.Content>
                     <Modal.Description>
+                        <Dropdown
+                            placeholder="Select a Game Title"
+                            fluid
+                            selection
+                            options={gameOptions}
+                            onChange={handleClose}
+                        />
+                        <Divider inverted />
                         <Form onSubmit={handleSubmit}>
                             <Form.Input
                                 name="title"
